1a028b4f978f8eb64678c3983cae14da8c89fd5e
Yura <smirnoff.yu@gmail.com>
2016-06-24 22:41:48 +0300

    html payment

--- qrpos-web/src/main/webapp/static/js/payment.js
+++ qrpos-web/src/main/webapp/static/js/payment.js
@@ -1,23 +1,55 @@
 $(document).ready(function(){
-
-	$("#payBtn").click(function(){
+    
+    var orderId = location.search.split('orderId=')[1];
+    $('#orderId').text(orderId);
+    
+    $.ajax({
+        url: "order/sessionStatus",
+        type: 'POST',
+        data: {
+            orderId: orderId
+        }
+    }).done(function(result) {
+        if (result.orderStatus == 'PAID') {
+            $('#payBtn').attr("disabled", true);
+            $('#state').addClass('successful');
+            $('#state').text('Заказ уже оплачен');
+        }
+        $('#amount').text(result.amount / 100);
+    }).fail(function (data) {
+        $('#payBtn').attr("disabled", true);
+        $('#state').addClass('error');
+        $('#state').text('Ошибка связи с сервером');
+    });
+    
+    $("#payBtn").click(function(){
+        $('#state').addClass('attention');
+        $('#state').text('Ожидание ответа');
+        $('#payBtn').attr("disabled", true);
 		$.ajax({
-			url: "qwe.ddns.net/qrapi/order/payment",
+			url: "order/payment",
 			type: 'POST',
 			data: {
-				orderId: location.search.split('orderId=')[1],
+				orderId: orderId,
 				paymentWay: 'card',
 				pan: $('#pan').val(),
 				cvc: $('#cvc').val(),
-				mont: $('#mont').val(),
+				month: $('#month').val(),
 				year: $('#year').val(),
 				cardHolderName: $('#cardHolderName').val()
 			}
 		}).done(function(result) {
-			debugger;
-			console.log(result);
-		});
-
+            if (result.orderStatus == 'PAID') {
+                $('#state').addClass('successful');
+                $('#state').text('Успешная оплата');
+            } else {
+                $('#state').addClass('error');
+                $('#state').text('Ошибка оплаты');
+            }
+        }).fail(function (data) {
+            $('#state').addClass('error');
+            $('#state').text('Ошибка оплаты');
+        });
 	});
 
 });
