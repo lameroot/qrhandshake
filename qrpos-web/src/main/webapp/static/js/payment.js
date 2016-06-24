10cf6dc1ddc5042b48bee4f26ab76f0a2f367a01
smirnov <y.smirnov@bpcbt.com>
2016-06-24 20:05:55 +0300

    fix sattic path

--- qrpos-web/src/main/webapp/static/js/payment.js
+++ qrpos-web/src/main/webapp/static/js/payment.js
@@ -1,3 +1,23 @@
-/**
- * Created by Smirnov_Y on 21.06.2016.
- */
+$(document).ready(function(){
+
+	$("#payBtn").click(function(){
+		$.ajax({
+			url: "qwe.ddns.net/qrapi/order/payment",
+			type: 'POST',
+			data: {
+				orderId: location.search.split('orderId=')[1],
+				paymentWay: 'card',
+				pan: $('#pan').val(),
+				cvc: $('#cvc').val(),
+				mont: $('#mont').val(),
+				year: $('#year').val(),
+				cardHolderName: $('#cardHolderName').val()
+			}
+		}).done(function(result) {
+			debugger;
+			console.log(result);
+		});
+
+	});
+
+});
