773946668b88b1d9d04d74e4d8a5f7bea6dfe113
lameroot <lameroot@mail.ru>
2016-10-19 08:48:30 +0300

    rbs to module

---
+++ qrpos-commons/src/main/java/ru/qrhandshake/qrpos/domain/BlackClient.java
@@ -0,0 +1,40 @@
+package ru.qrhandshake.qrpos.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "black_client")
+public class BlackClient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "blackClientSequence")
+    @SequenceGenerator(name = "blackClientSequence", sequenceName = "seq_black_client", allocationSize = 1)
+    private Long id;
+    private Long clientId;
+    private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
--- qrpos-web/src/main/java/ru/qrhandshake/qrpos/config/IntegrationConfig.java
+++ qrpos-web/src/main/java/ru/qrhandshake/qrpos/config/IntegrationConfig.java
@@ -1,18 +1,34 @@
 package ru.qrhandshake.qrpos.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.Gateway;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
 
 import java.util.Collection;
 
 @Configuration
-@IntegrationComponentScan(value = {"ru.qrhandshake.qrpos.integration"})
+@EnableIntegration
+@IntegrationComponentScan(value = {"ru.qrhandshake.qrpos.integration","ru.qrhandshake.qrpos.config"})
 public class IntegrationConfig {
 
     @MessagingGateway
-    public interface Upcase {
+    public interface IntegrationTest {
 
-        //https://spring.io/blog/2014/11/25/spring-integration-java-dsl-line-by-line-tutorial
+        @Gateway(requestChannel = "upcase.input")
+        Collection<String> upcase(Collection<String> strings);
+
+    }
+
+    @Bean
+    public IntegrationFlow upcase() {
+        return f -> f
+                .split()                                         // 1
+                .<String, String>transform(String::toUpperCase)  // 2
+                .aggregate();                                    // 3
     }
 }
